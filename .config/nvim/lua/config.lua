a81eb87edda3db06ed66dd2e823910df99beb2d6
Colby Morrison <colbyamorrison@gmail.com>
2022-12-06 23:33:18 -0700

    small lua update

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -13,14 +13,6 @@ vim.diagnostic.config({
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
 
-vim.keymap.set('n', '<leader>o', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '<leader>j', function() vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR}) end, opts)
-vim.keymap.set('n', '<leader>k', function() vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR}) end, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -30,6 +22,13 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', '<leader>o', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<leader>j', function() vim.diagnostic.goto_prev({severity = vim.diagnostic.severity.ERROR}) end, opts)
+  vim.keymap.set('n', '<leader>k', function() vim.diagnostic.goto_next({severity = vim.diagnostic.severity.ERROR}) end, opts)
+  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
@@ -138,6 +137,15 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['tsserver'].setup {
+  capabilities = capabilities
+}
+require('lspconfig')['pylsp'].setup {
+  capabilities = capabilities
+}
+require('lspconfig')['sqlls'].setup {
+  capabilities = capabilities
+}
+require('lspconfig')['sourcekit'].setup {
   capabilities = capabilities
 }
 
